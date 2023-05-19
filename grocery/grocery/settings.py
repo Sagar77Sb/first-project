760b9f141feea31d257fc5f5cc2fc7b6563e5adc
sagar <baduresagar77@gmail.com>
2023-05-19 21:59:16 +0530

    cartapp added

--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'groapp',
+    'cartapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
