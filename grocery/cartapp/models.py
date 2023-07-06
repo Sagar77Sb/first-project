1273f224cf1c559023ce6959cde8866bc314577e
sagar <baduresagar77@gmail.com>
2023-07-06 23:02:22 +0530

    Cart Model Created

--- grocery/cartapp/models.py
+++ grocery/cartapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=200)
+    price=models.IntegerField()
+    picture=models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.IntegerField(default=0)
+    amount=models.IntegerField(default=0)
+    total_amt=models.IntegerField(default=0)
+
+    def total_cost(self):
+        return self.quantity*self.product.price
+    
+class CartItem(forms.ModelForm):
+    class Meta:
+        model=Cart
+        fields='__all__'
--- grocery/grocery/urls.py
+++ grocery/grocery/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from groapp.views import home
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",home),
     path("kitchen-",include(("groapp.urls","groapp"),namespace="apps")),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
