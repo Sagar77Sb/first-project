945c8655a631ee381e7a6f44821e374f1ddd6512
sagar <baduresagar77@gmail.com>
2023-07-07 23:27:35 +0530

    product page complete

--- grocery/cartapp/urls.py
+++ grocery/cartapp/urls.py
@@ -3,5 +3,7 @@ from .import views as v
 app_name='cart'
 urlpatterns =[
     path('products',v.productt,name='product'),
-    path('addtocart/<int:pk>',v.add_cart,name='addtocart1')
+    path('addtocart/<int:pk>',v.add_cart,name='addtocart1'),
+    path('showcart',v.show_cart,name='showcart1'),
+    path("del/<int:proid>",v.delete,name='delete1')
 ]
--- grocery/cartapp/views.py
+++ grocery/cartapp/views.py
@@ -15,3 +15,23 @@ def add_cart(request,pk):
         Cart(user=user,product=product).save()
         return redirect("/cartapp-products")
 
+def show_cart(request):
+    ids=request.session.get("id")
+    obj=Cart.objects.filter(id=ids)
+    amount=0
+    value=0
+    for p in obj:
+        value=p.quantity*p.product.price
+        amount=value+amount
+        total_amount= amount + 45
+        d={'data':obj,'total_amount':total_amount,'amount':amount}
+        return render(request,'carts.html',d)
+    
+def delete(request,proid):
+    obj=Cart.objects.get(id=proid)
+    obj.delete()
+    return redirect("/cartapp-showcart")
+
+
+
+
